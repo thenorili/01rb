454d0e7b1e53d5261c4f279c21717e5d4b390bc7
nori li <50680474+thenorili@users.noreply.github.com>
2020-01-31 01:50:50 -0500

    Add files via upload

---
+++ HailstoneSequence.rb
@@ -0,0 +1,21 @@
+#!/home/nori/.rubies/ruby-2.7.0/bin ruby
+
+def HailstoneSequence(n)
+  if n.is_a?(Integer) && n > 1 
+    puts "Hailstone Sequence for \n #{n}"
+    hail_seq = Array.new
+    hail_seq << n
+    while n != 1
+      if n % 2 == 0
+        n = n / 2
+      else
+        n = 3 * n + 1
+      end
+      hail_seq << n
+    end
+  puts "#{hail_seq}"
+  return hail_seq
+  else
+    puts "An integer greater than one must be supplied as an argument."
+  end
+end
